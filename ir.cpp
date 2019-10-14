75af61c3201594e8241dcfb6be2d9e2525a1f23a
zchoffma <zchoffma@asu.edu>
2019-10-13 17:41:13 -0700

    added cpp impl of ir_test

---
+++ ir.cpp
@@ -0,0 +1,36 @@
+#include <wiringSerial.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <errno.h>
+
+int fd = serialOpen("/dev/ttyACM0", 9600);
+int bytesToRead;
+char serialData[];
+
+int main(){
+    wiringPiSetup();
+    
+    if(fd > 0){
+        serialFlush(fd); //flush any data in buffer
+        printf("[IR-TEST] begin pressing buttons\n");
+
+	    while(1){
+            bytesToRead = serialDataAvail(fd);
+            
+	        if(bytesToRead > 0){
+                serialData = new char[bytesToRead];
+	            
+                for(int i = 0; i < bytesToRead; i++){
+                    printf("%c", serialData[i]);
+                }
+
+                printf("\n");
+                delete serialData;
+            }
+        } 
+    }else{
+        printf("[ERROR] unable to open port '/dev/ttyACM0'\n");
+    }
+
+    serialClose(fd);
+}
